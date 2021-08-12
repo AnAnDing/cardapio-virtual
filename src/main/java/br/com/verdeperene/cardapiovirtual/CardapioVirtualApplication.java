c77a3d39f21bc6a4d1c0c713682c79ec2d123abe
César Dick <cesardickdasilva@gmail.com>
2021-08-11 21:13:23 -0300

    Criação do Projeto

---
+++ src/main/java/br/com/verdeperene/cardapiovirtual/CardapioVirtualApplication.java
@@ -0,0 +1,13 @@
+package br.com.verdeperene.cardapiovirtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardapioVirtualApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CardapioVirtualApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/verdeperene/cardapiovirtual/CardapioVirtualApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.verdeperene.cardapiovirtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardapioVirtualApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
